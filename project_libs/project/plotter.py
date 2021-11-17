6008a7ea0fb38a6a4a3829b7848690ebbb58b61e
drkostas <georgiou.kostas94@gmail.com>
2021-11-17 12:15:31 -0500

    Heatmap visualization for the correlations between columns #4

--- project_libs/project/plotter.py
+++ project_libs/project/plotter.py
@@ -1,2 +1,14 @@
+from typing import *
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
+
+def viz_columns_corr(df: pd.DataFrame, cols_to_visualize: List[str]) -> None:
+    df_ = df.copy()
+    df_ = df_[cols_to_visualize].rename(lambda x: x[:20] + '..' if len(x) > 22 else x,
+                                        axis='columns')
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 7))
+    sns.set(font_scale=1.4)
+    sns.heatmap(data=df_.corr(), cmap='coolwarm', annot=True, fmt=".1f",
+                annot_kws={'size': 16}, ax=ax)
--- project_libs/project/preprocessing.py
+++ project_libs/project/preprocessing.py
@@ -8,10 +8,10 @@ from numpy import NaN
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
+
 def isolate_city_state(data, cities, states):
     """ This ensures that each city is selected with it's respective state
     which is why I didn't simply run a merge statement. """
-
 
     for ind, x in enumerate(zip(cities, states)):
         tmp = data.loc[(data['City'] == x[0]) & (data['State'] == x[1])].copy()
@@ -47,7 +47,6 @@ def basic_impute(data):
 
 
 def encode_std_extract_split(X):
-
     # Dropping the following features
     # 1. Civil_Twilight        - Use Sunrise_Sunset instead
     # 2. Nautical_Twilight     - Use Sunrise_Sunset instead
@@ -76,25 +75,24 @@ def encode_std_extract_split(X):
 
     # Label Encoding
     binary_features = [
-                        'Amenity','Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit', 'Railway',
-                        'Station', 'Stop', 'Traffic_Calming', 'Traffic_Signal', 'Side', 'Turning_Loop',
-                        'Sunrise_Sunset'
-                    ]
+        'Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit', 'Railway',
+        'Station', 'Stop', 'Traffic_Calming', 'Traffic_Signal', 'Side', 'Turning_Loop',
+        'Sunrise_Sunset'
+    ]
 
     for features in binary_features:
         le = LabelEncoder()
         X[features] = le.fit_transform(X[features])
 
-
     # Create duration feature
     X['Start_Time'] = pd.to_datetime(X['Start_Time'])
     X['End_Time'] = pd.to_datetime(X['End_Time'])
-    X['Duration'] = (X['End_Time'] - X['Start_Time']).dt.total_seconds() / 60.0 # Minutes
+    X['Duration'] = (X['End_Time'] - X['Start_Time']).dt.total_seconds() / 60.0  # Minutes
 
     # Create month, weekday, and hour feature
     X["Month"] = X["Start_Time"].dt.month
-    X["DayOfWeek"]  = X["Start_Time"].dt.dayofweek
-    X["Hour"]  = X["Start_Time"].dt.hour
+    X["DayOfWeek"] = X["Start_Time"].dt.dayofweek
+    X["Hour"] = X["Start_Time"].dt.hour
 
     # Create Cartesian Coordinates
     X["Coord_X"] = np.cos(X["Start_Lat"] * np.cos(X["Start_Lng"]))
@@ -113,40 +111,42 @@ def encode_std_extract_split(X):
     timezone_df = pd.get_dummies(X['Hour'], prefix='Hour')
     wind_direction_df = pd.get_dummies(X['Wind_Direction'], prefix='Wind_Direction')
     weather_condition_df = pd.get_dummies(X['Weather_Condition'], prefix='Weather_Condition')
-    X = pd.concat([X, city_df, county_df, timezone_df, wind_direction_df, weather_condition_df], axis=1)
+    X = pd.concat([X, city_df, county_df, timezone_df, wind_direction_df, weather_condition_df],
+                  axis=1)
 
     # Drop non essential features
     non_essential_features = [
-                                'ID', 'Severity', 'Start_Time', 'End_Time', 'Start_Lat', 'Start_Lng','End_Lat',
-                                'End_Lng', 'Description', 'Number', 'Street', 'State', 'Zipcode', 'Country',
-                                'Airport_Code', 'Weather_Timestamp', 'Roundabout','Civil_Twilight', 'Nautical_Twilight',
-                                'Astronomical_Twilight'
-                            ]
+        'ID', 'Severity', 'Start_Time', 'End_Time', 'Start_Lat', 'Start_Lng', 'End_Lat',
+        'End_Lng', 'Description', 'Number', 'Street', 'State', 'Zipcode', 'Country',
+        'Airport_Code', 'Weather_Timestamp', 'Roundabout', 'Civil_Twilight', 'Nautical_Twilight',
+        'Astronomical_Twilight'
+    ]
     X.drop(non_essential_features, axis=1, inplace=True)
 
     # Drop one hot encoded features
-    one_hot = ['City', 'County', 'Timezone', 'Month', 'DayOfWeek', 'Hour', 'Wind_Direction', 'Weather_Condition' ]
+    one_hot = ['City', 'County', 'Timezone', 'Month', 'DayOfWeek', 'Hour', 'Wind_Direction',
+               'Weather_Condition']
     X.drop(one_hot, axis=1, inplace=True)
 
     # Train Test Split
     X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                            test_size=0.10, shuffle=True, stratify=y, random_state=42)
+                                                        test_size=0.10, shuffle=True, stratify=y,
+                                                        random_state=42)
 
     # Standardize Numerical Variables
     numerical_features = [
-                          'Distance(mi)', 'Temperature(F)', 'Wind_Chill(F)',
-                          'Humidity(%)', 'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)',
-                          'Precipitation(in)', 'Duration', 'Coord_X', 'Coord_Y', 'Coord_Z'
-                        ]
+        'Distance(mi)', 'Temperature(F)', 'Wind_Chill(F)',
+        'Humidity(%)', 'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)',
+        'Precipitation(in)', 'Duration', 'Coord_X', 'Coord_Y', 'Coord_Z'
+    ]
 
     for feature in numerical_features:
         mu = X_train[feature].mean()
         sigma = X_train[feature].std()
-        X_train[feature] = X_train[feature].apply(lambda x: (x-mu)/sigma)
-        X_test[feature]  = X_test[feature].apply(lambda x: (x-mu)/sigma)
+        X_train[feature] = X_train[feature].apply(lambda x: (x - mu) / sigma)
+        X_test[feature] = X_test[feature].apply(lambda x: (x - mu) / sigma)
 
     return (X_train, X_test, y_train, y_test)
-
 
 
 def preprocess_loc_basic_var(accident_df):
